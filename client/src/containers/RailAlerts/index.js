edb70a5a65944d61e59ab2919bd3e50eace7e9f5
James Collins <james@jcoll-mbp115.local>
2018-06-10 22:34:48 -0400

    Line toggle and alert toggle

--- client/src/containers/RailAlerts/index.js
+++ client/src/containers/RailAlerts/index.js
@@ -5,12 +5,61 @@ import moment from 'moment';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import './style.scss';
-
+import ReactDOMServer from 'react-dom/server';
 const KNOWN_ALERT_TYPES = ['Alert', 'Delay'];
 
-const filterAffectedLines = linesAffected =>
+const PHRASES = [];
+
+LINE_NAMES.forEach(n => {
+  PHRASES.push({
+    phrase: `${n} line`,
+    style: { backgroundColor: LINE_PROPERTIES[n]['color'] }
+  });
+  PHRASES.push({
+    phrase: n,
+    style: { backgroundColor: LINE_PROPERTIES[n]['color'] }
+  });
+});
+
+const getAffectedLineCodes = linesAffected =>
   linesAffected.split(/;[\s]?/).filter(l => l !== '');
+
 class RailAlert extends React.Component {
+  state = {
+    htmlDescription: ''
+  };
+
+  componentWillMount() {
+    const { Description } = this.props;
+    let htmlDescription = Description;
+    PHRASES.forEach(({ phrase, style }) => {
+      htmlDescription = htmlDescription.replace(
+        new RegExp(phrase, 'gi'),
+        ReactDOMServer.renderToStaticMarkup(<span style={style}>{phrase}</span>)
+      );
+    });
+    this.setState({ htmlDescription });
+  }
+
+  getIndicesOf = (searchStr, str, caseSensitive) => {
+    const searchStrLen = searchStr.length;
+    if (searchStrLen == 0) {
+      return [];
+    }
+    let startIndex = 0;
+    let index = 0;
+    const indices = [];
+    if (!caseSensitive) {
+      str = str.toLowerCase();
+      searchStr = searchStr.toLowerCase();
+    }
+    while ((index = str.indexOf(searchStr, startIndex)) > -1) {
+      startIndex = index + searchStrLen;
+      indices.push([index, startIndex]);
+    }
+    return indices;
+  };
+
   render() {
     const {
       IncidentID,
@@ -19,8 +68,8 @@ class RailAlert extends React.Component {
       LinesAffected,
       DateUpdated
     } = this.props;
-    const affectedLines = filterAffectedLines(LinesAffected);
-    const desc = Description.toLowerCase();
+    const { htmlDescription } = this.state;
+    const affectedLines = getAffectedLineCodes(LinesAffected);
     const alert = {
       lines: LINE_NAMES.filter(l =>
         affectedLines.includes(LINE_PROPERTIES[l]['code'])
@@ -51,6 +100,7 @@ class RailAlert extends React.Component {
             {'(' + moment(DateUpdated).fromNow() + ')'}
           </div>
         </div>
+        {/* <div dangerouslySetInnerHTML={{ __html: htmlDescription }} className="description"/> */}
         <div className="description">{Description}</div>
       </div>
     );
@@ -58,21 +108,79 @@ class RailAlert extends React.Component {
 }
 
 class RailAlerts extends React.Component {
+  state = {
+    expanded: false,
+    manuallyControlled: false, //has the user interacted with the alerts button?
+    hasNewUnreadAlerts: false
+  };
   componentWillMount() {
     const { fetchRailAlerts } = this.props;
     fetchRailAlerts();
     setInterval(fetchRailAlerts, 12000);
   }
 
+  componentWillReceiveProps(nextProps) {
+    const { railAlerts } = nextProps;
+    const { manuallyControlled, expanded, hasNewUnreadAlerts } = this.state;
+    if (!railAlerts || railAlerts.length === 0) {
+      this.setState({ hasNewUnreadAlerts: false });
+    } else if (!manuallyControlled && railAlerts !== this.props.railAlerts) {
+      this.setState({ expanded: true });
+    } else if (
+      railAlerts !== this.props.railAlerts &&
+      !expanded &&
+      !hasNewUnreadAlerts
+    ) {
+      const newAlertIDs = railAlerts.map(({ IncidentID }) => IncidentID);
+      const oldAlertIDs = this.props.railAlerts.map(
+        ({ IncidentID }) => IncidentID
+      );
+      this.setState({
+        hasNewUnreadAlerts: !newAlertIDs.every(a => oldAlertIDs.includes(a))
+      });
+    }
+  }
+
   render() {
     const { railAlerts } = this.props;
+    const { expanded, hasNewUnreadAlerts } = this.state;
+
     return (
       <div className="RailAlerts">
-        {railAlerts &&
-          railAlerts.length > 0 &&
-          railAlerts.map((alert, index) => (
-            <RailAlert {...alert} key={alert['IncidentID']} index={index} />
-          ))}
+        <div>
+          <div
+            className={`expand-button${expanded ? ' expanded' : ' closed'}`}
+            onClick={() =>
+              this.setState({
+                expanded: !expanded,
+                manuallyControlled: true,
+                hasNewUnreadAlerts: false
+              })
+            }
+          />
+          <div
+            className="alerts-container"
+            style={{
+              maxHeight: expanded ? '' : '0px',
+              width: expanded ? '' : '0px'
+            }}>
+            {railAlerts &&
+              railAlerts.length > 0 &&
+              railAlerts.map((alert, index) => (
+                <RailAlert {...alert} key={alert['IncidentID']} />
+              ))}
+            {(!railAlerts || railAlerts.length === 0) && (
+              <div className="no-alerts">
+                WMATA is not reporting any problems at this time :)
+              </div>
+            )}
+          </div>
+          <div
+            className={`expand-button-shadow${
+              hasNewUnreadAlerts ? ' new-alerts' : ''
+            }`}
+          />
+        </div>
       </div>
     );
   }
--- client/src/reducers/controls.js
+++ client/src/reducers/controls.js
@@ -1,6 +1,7 @@
 import { SET_VISIBLE_RAIL_LINES } from '../actions/controls';
+import { LINE_NAMES } from 'common/constants/lines';
 
-const initialVisibleRailLines = [];
+const initialVisibleRailLines = [].concat(LINE_NAMES);
 
 const visibleRailLines = (state = initialVisibleRailLines, action) => {
   switch (action.type) {

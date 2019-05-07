ca0d41fe26697567c13584be95006a2474e1746a
James Collins <jbccollins@gmail.com>
2019-05-07 09:18:12 -0400

    Move port conflict resolution out of server.js

---
+++ runApp.js
@@ -0,0 +1,42 @@
+import find from 'find-process';
+import chalk from 'chalk';
+
+const runApp = (app, port) => {
+  let portConflictResolutionAttempted = false;
+  const start = () => {
+    app.listen(port, () => console.log(chalk.green(`[server] Listening on port ${port}`)))
+        .on('error', e => {
+          if (!portConflictResolutionAttempted) {
+            console.log(chalk.yellow(`[server] Encountered a port conflict. Attempting to clear port ${port}.`))
+            killConflictingApps();
+          } else {
+            console.log(chalk.red(`[server] Unable to listen on port ${port}:`));
+            console.log(chalk.red(e));
+          }
+        });
+  }
+  const killConflictingApps = async () => {
+    portConflictResolutionAttempted = true;
+    if (process.env.NODE_ENV !== "production") {
+      const list = await find('port', port);
+      if (!list.length) {
+        console.log(chalk.green(`[server] Port ${port} is free :)`));
+      } else {
+        list.forEach(({pid, name}) => {
+          if (pid !== process.pid) {
+            console.log(chalk.cyan(`[server] Killing process ${name} due to conflict on port ${port}.`));
+            process.kill(pid);
+          }
+        })
+      }
+      // process.kill does not happen immediately so we need to wait just a bit before
+      // actually trying to run again.
+      setTimeout(start, 100);
+    } else {
+      console.log(chalk.red('[server] Not killing conflicting apps because in production mode.'));
+    }
+  }
+  start();
+}
+
+export default runApp;
--- server.js
+++ server.js
@@ -1,50 +1,14 @@
 import express from 'express';
 import path from 'path';
-import find from 'find-process';
-import chalk from 'chalk';
 import dotenv from 'dotenv';
+import runApp from './runApp';
 dotenv.config();
 
 const app = express();
 const DEFAULT_PORT = 5001;
 const port = process.env.PORT || DEFAULT_PORT;
-let portConflictResolutionAttempted = false;
 
-const runApp = () => {
-  app.listen(port, () => console.log(chalk.green(`[server] Listening on port ${port}`)))
-      .on('error', e => {
-        if (!portConflictResolutionAttempted) {
-          console.log(chalk.yellow(`[server] Encountered a port conflict. Attempting to clear port ${port}.`))
-          killConflictingApps();
-        } else {
-          console.log(chalk.red(`[server] Unable to listen on port ${port}:`));
-          console.log(chalk.red(e));
-        }
-      });
-}
-const killConflictingApps = async () => {
-  portConflictResolutionAttempted = true;
-  if (process.env.NODE_ENV !== "production") {
-    const list = await find('port', port);
-    if (!list.length) {
-      console.log(chalk.green(`[server] Port ${port} is free :)`));
-    } else {
-      list.forEach(({pid, name}) => {
-        if (pid !== process.pid) {
-          console.log(chalk.cyan(`[server] Killing process ${name} due to conflict on port ${port}.`));
-          process.kill(pid);
-        }
-      })
-    }
-    // process.kill does not happen immediately so we need to wait just a bit before
-    // actually trying to run again.
-    setTimeout(runApp, 100);
-  } else {
-    console.log(chalk.red('[server] Not killing conflicting apps because in production mode.'));
-  }
-}
-
-runApp();
+runApp(app, port);
 
 app.use(express.static(__dirname + '/client/build'));
 
